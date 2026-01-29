df0241cb8285b49c3b499d49676064b6e5a5136c
TobiasVoegeleEdeka <tobias.voegele@edeka.de>
2026-01-29 18:35:31 +0100

    n

--- spring/src/main/java/com/example/demo/MailGrpcController.java
+++ spring/src/main/java/com/example/demo/MailGrpcController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.nio.charset.StandardCharsets;
 
 @RestController
-public class MailTestController {
+public class MailGrpcController {
 
     @GrpcClient("mailServer")
     private MailGatewayServiceGrpc.MailGatewayServiceBlockingStub mailStub;
